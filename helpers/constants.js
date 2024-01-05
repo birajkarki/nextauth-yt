ecdefa6cda594b816c872381b48971f2c4ba1b13
Biraj Karki <birajkarki9849@gmail.com>
2024-01-06 01:22:05 +0545

    update

---
+++ helpers/constants.js
@@ -0,0 +1,14 @@
+const users = [
+  {
+    id: "1",
+    name: "Biraj",
+    email: "birajkarki@gmail.com",
+    password: "123456",
+  },
+  {
+    id: "2",
+    name: "Subscrib",
+    email: "birajkarki@gmail.com",
+    password: "123456",
+  },
+];
